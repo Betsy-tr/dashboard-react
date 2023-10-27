391c80fcf9413161cbfced78272ba1f028f7ad4d
betsy-tr <91901227+Betsy-tr@users.noreply.github.com>
2023-10-27 13:16:49 -0400

    création des pages

---
+++ src/pages/Private/truthOrDare/Ajouter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ajouter = () => {
+  return (
+    <div>Ajouter</div>
+  )
+}
+
+export default Ajouter
---
+++ src/pages/Private/truthOrDare/Lister.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Lister = () => {
+  return (
+    <div>Lister</div>
+  )
+}
+
+export default Lister
