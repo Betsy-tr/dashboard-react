4a1e9576031779af6ce02237cb0759d542bad9e1
betsy-tr <91901227+Betsy-tr@users.noreply.github.com>
2023-11-08 16:46:07 -0400

    formulaire

--- src/pages/Public/Index.jsx
+++ src/pages/Public/Index.jsx
@@ -1,8 +1,67 @@
 import React from 'react'
+import { loginByEmail , logout } from '../../fonctionsjs/auth'
+import { useForm } from 'react-hook-form'
 
 const Index = () => {
+
+  const {register , handleSubmit , formState:{errors} } = useForm()
+
+  const login = async (data) => { 
+        
+    //event.preventDefault()
+    console.log(data)
+
+    const {email , password } = data
+    
+    loginByEmail(email , password).then(user =>{
+
+    }).catch(error =>{
+      console.log('error' , error)
+    })
+  }
+
   return (
-    <div>Index</div>
+    <div>
+      <div>
+        <div>
+
+          <h1>DASHBOARD</h1>
+
+          <div>
+            <h2>Heureux de te revoir !</h2>
+          </div>
+
+          <form onSubmit={handleSubmit(login)}>
+
+            <div>
+              <label>
+                <span>Email</span>
+              </label>
+              <input type="text" name='email' {...register('email' , {required: true})} placeholder="Email"/>
+              {errors.email && <label>
+                <span>L'email est requis</span>
+              </label>}
+            </div>
+
+            <div>
+              <label>
+                <span>Password</span>
+              </label>
+              <input type="password" name='password' {...register('password' , {required: true})} placeholder="Mot de passe"/>
+              {errors.password && <label>
+                <span>Le mot de passe est requis</span>
+              </label>}
+            </div>
+
+            <div>
+              <button type='submit'>Se connecter</button>
+            </div>
+                                
+          </form>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
