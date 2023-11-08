5a846a3212c2da847a6710d42c60a14b1369da18
betsy-tr <91901227+Betsy-tr@users.noreply.github.com>
2023-11-08 18:01:27 -0400

    configuration du fichier App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import Private from './pages/Private/Index'
+import { onAuthStateChanged } from '@firebase/auth'
+import { auth } from '../firebase'
+import { initUser, resetUser } from './redux/userStore'
+
 
 const App = () => {
+
+  const dispatch = useDispatch() ; 
+
+  const userID = useSelector(state => state.user)
+
+  onAuthStateChanged(auth , user =>{
+
+    if (user) {
+
+      dispatch(initUser(user.uid))
+
+    }else{
+
+      dispatch(resetUser())
+
+    }
+  })
+
   return (
 
-    <Private/>
+    <>
+
+      {userID ? <Private/> : <Public/>}
+    
+    </>
 
   )
 }
